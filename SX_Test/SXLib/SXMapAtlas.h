a5b15737c2c98a27f52b4cabebe3012d60a572c4
loic abadie <blaackjack@hotmail.com>
2013-10-19 14:32:10 +0200

    initial repo

---
+++ SX_Test/SXLib/SXMapAtlas.h
@@ -0,0 +1,13 @@
+//
+//  SXMapAtlas.h
+//  SX_Test
+//
+//  Created by Abadie Loic on 19/10/13.
+//  Copyright (c) 2013 Abadie Loic. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SXMapAtlas : NSObject
+
+@end
