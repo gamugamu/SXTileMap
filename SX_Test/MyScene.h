198e1bf1e426e3077e0af55edf4ac4b2b6a1d11c
loic abadie <blaackjack@hotmail.com>
2013-10-19 15:11:42 +0200

    project integration

---
+++ SX_Test/MyScene.h
@@ -0,0 +1,13 @@
+//
+//  MyScene.h
+//  SX_Test
+//
+
+//  Copyright (c) 2013 Abadie Loic. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface MyScene : SKScene
+
+@end
---
+++ SX_TestTests/SX_TestTests.m
@@ -0,0 +1,34 @@
+//
+//  SX_TestTests.m
+//  SX_TestTests
+//
+//  Created by Abadie Loic on 19/10/13.
+//  Copyright (c) 2013 Abadie Loic. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface SX_TestTests : XCTestCase
+
+@end
+
+@implementation SX_TestTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
