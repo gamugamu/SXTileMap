ba3564064073c52e5e9518899acad34ec546703f
loic abadie <blaackjack@hotmail.com>
2013-10-19 15:48:36 +0200

    adding interface for SXMutableMapAtlas

--- SXTileMap/SXLib/SXMutableMapAtlas.h
+++ SXTileMap/SXLib/SXMutableMapAtlas.h
@@ -8,6 +8,21 @@
 
 #import "SXMapAtlas.h"
 
+//----------------------------------------------------------------------------//
+// SXMapAtlas holds a mutable collection of tiles.
+//----------------------------------------------------------------------------//
 @interface SXMutableMapAtlas : SXMapAtlas
+
+- (void)addTileAtPoint:(void*)pnt;
+- (void)removeTileAtPoint:(void*)pnt;
+
+- (void)addTilesFromRegion:(void*)region;
+- (void)removeTilesFromRegion:(void*)region;
+
+- (void)addTilesFromColumn:(void*)region;
+- (void)addTilesFromRow:(void*)region;
+
+- (void)removeTilesFromColumn:(void*)column;
+- (void)removeTilesFromRow:(void*)row;
 
 @end
