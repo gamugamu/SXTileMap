1d3836bb6ecdc6bf4599cf15250176528821f7be
loic abadie <blaackjack@hotmail.com>
2013-10-20 14:18:39 +0200

    defining opaque SX stuct

--- SXMapAtlasDescription.h
+++ SXMapAtlasDescription.h
@@ -12,6 +12,38 @@
 // SXMapAtlasDescription define what is a MapAtlas file.
 //----------------------------------------------------------------------------//
 
+// A with and height of a mapGrid.
+typedef struct{
+    uint_fast8_t row;
+    uint_fast8_t column;
+}SXGridSize;
+
+// Represent the size of all tiles into a SXMapAtlas. They are all of the same
+// size. Note that a square is not required to be a square size.
+typedef struct{
+    uint_fast8_t row;
+    uint_fast8_t column;
+}SXTileSize;
+
+// Represent the coordinate system.
+typedef struct{
+    uint_fast8_t x;
+    uint_fast8_t y;
+}SXPoint;
+
+typedef struct{
+    UInt32 tid;         // a unique id for each tile.
+    SXPoint position;   // where the tile is into the map coordinate.
+}SXTile;
+
+typedef struct{
+    SXGridSize sizeGrid;
+    SXTileSize sizeTile;
+    void** tiles;
+}mapDescription;
+
+// **** Note: they must be opaque and hidden from the implementation.
+
 @interface SXMapAtlasDescription : NSObject
 
 + (id)mapAtlasDescription:(NSString*)fileName;
